4e352c2007d4fc2832f126fd8c743eada1b41232
Marcus Kelly <mek413@gmail.com>
2020-01-10 18:02:47 -0600

    completed app functionality and runs as expected

---
+++ models/book.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  googleId: String,
+  title: { type: String, required: true},
+  subtitle: String,
+  authors: Array,
+  description: String,
+  image: String,
+  link: String,
+  date: {type: Date, default:Date.now}
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
